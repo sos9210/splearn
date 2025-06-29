83683b4976460e3561561c2cdce2c95246a4fe66
sungsanghyun <sos9210@naver.com>
2025-06-29 23:21:54 +0900

    project init

---
+++ reehi/src/test/java/com/reehi/ReehiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reehi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReehiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
